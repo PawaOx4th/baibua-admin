74b08eac2539f780925cf08d9142a2a9440ebe92
Pawanachai <Kanpim5@gmail.com>
2020-03-07 18:02:20 +0700

    -

---
+++ src/components/AllSubject.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="allsubject">
+    <v-btn color="success" @click="fechSubject">text</v-btn>
+
+    <v-container
+      class=" d-flex justify-start align-center flex-wrap pa-5"
+      fluid
+    >
+      <!-- <v-col> -->
+
+      <!-- <v-col md="4" class="red"> -->
+
+      <v-card
+        v-for="(subject, index) in data"
+        :key="index"
+        width="450"
+        height="200"
+        :class="`subject ${subject.Type == 0 ? `open` : `close`}  pa-5  ma-1`"
+      >
+        <!-- //* Name -->
+        <v-row row wrap>
+          <v-col md="12" xs="12">
+            <div class=" grey--text text--lighten-1 ma-0">ชื่อวิชา</div>
+            {{ subject.NameTH }}
+            <div>{{ subject.NameEN }}</div>
+          </v-col>
+        </v-row>
+
+        <!-- //*  -->
+      </v-card>
+
+      <!-- </v-col> -->
+
+      <!-- </v-col> -->
+    </v-container>
+    <!-- </v-hover> -->
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "allsubject",
+  data() {
+    return {
+      data: []
+    };
+  },
+  created() {
+    this.fechSubject();
+  },
+  methods: {
+    async fechSubject() {
+      const url =
+        "https://us-central1-newagent-47c20.cloudfunctions.net/api/subject";
+      await axios.get(url).then(val => {
+        console.dir(val);
+        this.data = val.data;
+        // console.log(this.data);
+      });
+    }
+  }
+};
+</script>
+
+<style>
+.subject.close {
+  border-left: 5px solid #f4511e;
+}
+
+.subject.open {
+  border-left: 5px solid #238aeb;
+}
+</style>
+
+:class="`pa-3 subject ${subject.Type === 1 ? `close` : `open`} `"
--- src/views/Page/Subject.vue
+++ src/views/Page/Subject.vue
@@ -3,17 +3,22 @@
     <div class="">
       <!-- //* Header -->
       <SubjectSearch />
+
+      <!-- //* Subject All -->
+      <AllSubject />
     </div>
   </div>
 </template>
 
 <script>
 import SubjectSearch from "@/components/Subject_search.vue";
+import AllSubject from "@/components/AllSubject.vue";
 
 export default {
   name: "subject",
   components: {
-    SubjectSearch
+    SubjectSearch,
+    AllSubject
   },
   data() {
     return {};
