3cf0d233c3e959cbf240c01d4a0cfb9d39808319
Pawanachai <Kanpim5@gmail.com>
2020-03-30 22:33:18 +0700

    Modify Sign in

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,52 +1,70 @@
 <template>
-  <!-- <div id="signin">
-    <hr />
-    <h1>Sign In</h1>
-    <form @submit.prevent="pressed">
-      
-      <v-text-field v-model.trim="email" type="email"></v-text-field>
-      <v-text-field v-model.trim="password" type="password"></v-text-field>
-      <v-btn color="info" type="submit">Sing In</v-btn>
-    </form>
-    
-  </div> -->
   <v-app id="inspire">
-    <div id="bglogin">
-      <v-container class="fill-height " fluid id="container">
-        <v-row align="center" justify="end">
-          <v-col cols="12" sm="12" md="4">
-            <v-hover v-slot:default="{ hover }">
-              <v-card class="elevation-12" :elevation="hover ? 12 : 2">
-                <v-toolbar color="#57BFFF" dark>
-                  <v-toolbar-title>Login Admin</v-toolbar-title>
-                </v-toolbar>
-                <v-card-text class="pa-10">
-                  <v-form @submit.prevent="pressed">
-                    <v-text-field
-                      label="Login"
-                      name="login"
-                      prepend-icon="person"
-                      type="text"
-                      v-model="email"
-                      class="ma-5"
-                    />
+    <!-- 
+      * 2 Sectionn 
+     -->
+    <v-row row wrap>
+      <!-- 
+        //  Section 1 picture  
+        //  Set Style in css id="left"
+        //  Hidden only on xs
+       -->
+      <v-col md="7" cols="12" class="d-none d-sm-flex" id="left">
+        <div></div>
+      </v-col>
 
-                    <v-text-field
-                      id="password"
-                      label="Password"
-                      name="password"
-                      prepend-icon="lock"
-                      type="password"
-                      v-model="password"
-                      class="ma-5"
-                      @keyup.enter="pressed"
-                    />
+      <!-- 
+        //  Section 2 Form Input
+       -->
+      <v-col md="5" cols="12" class="#DADBDD " align="Stretch" id="">
+        <v-container fill-height fluid>
+          <v-row align="center" justify="center">
+            <v-col align="center">
+              <v-card elevation="0" class="pa-5">
+                <!-- 
+                  * Logo
+                 -->
 
-                    <div class="error--text" v-if="error">
-                      {{ error.message }}
-                    </div>
-                  </v-form>
-                </v-card-text>
+                <v-img
+                  src="../assets/Logo/Baibua-Logo-horizontal.png"
+                  height="120px"
+                  contain
+                >
+                </v-img>
+
+                <!-- 
+                  *Title 
+                  -->
+                <div class="display-2 mt-5 font-weight-medium">SIGN IN</div>
+
+                <!-- 
+                  * Form
+                 -->
+                <v-form @submit.prevent="pressed">
+                  <v-text-field
+                    label="Login"
+                    name="login"
+                    prepend-icon="person"
+                    type="text"
+                    v-model="email"
+                    class="ma-5"
+                  />
+
+                  <v-text-field
+                    id="password"
+                    label="Password"
+                    name="password"
+                    prepend-icon="lock"
+                    type="password"
+                    v-model="password"
+                    class="ma-5"
+                    @keyup.enter="pressed"
+                  />
+
+                  <div class="error--text" v-if="error">
+                    {{ error.message }}
+                  </div>
+                </v-form>
                 <v-card-actions>
                   <v-spacer />
                   <v-btn
@@ -54,20 +72,79 @@
                     color="#57BFFF"
                     @click="pressed"
                     class="white--text"
+                    elevation="0"
+                    x-large
                     >Login</v-btn
                   >
-                  <!-- <v-btn color="primary" @click.alt="defultData">Login</v-btn> -->
                 </v-card-actions>
               </v-card>
-            </v-hover>
-          </v-col>
-        </v-row>
-      </v-container>
-      <v-overlay :value="overlay" opacity="0.8">
-        <v-img src="../assets/5.svg"> </v-img>
-        <!-- <v-img src="../assets/Spinner.gif"> </v-img> -->
-      </v-overlay>
-    </div>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-col>
+
+      <!-- 
+        // Section 2 Responsive
+       -->
+      <!-- <v-col
+        md="5"
+        cols="12"
+        class=" d-flex d-sm-none"
+        align="Stretch"
+        id="form"
+      >
+        <v-container fill-height fluid>
+          <v-row align="center" justify="center">
+            <v-col align="center">
+              <v-card elevation="0" class="pa-5" id="card">
+                <div class="display-2">Login</div>
+                <v-form @submit.prevent="pressed">
+                  <v-text-field
+                    label="Login"
+                    name="login"
+                    prepend-icon="person"
+                    type="text"
+                    v-model="email"
+                    class="ma-5"
+                  />
+
+                  <v-text-field
+                    id="password"
+                    label="Password"
+                    name="password"
+                    prepend-icon="lock"
+                    type="password"
+                    v-model="password"
+                    class="ma-5"
+                    @keyup.enter="pressed"
+                  />
+
+                  <div class="error--text" v-if="error">
+                    {{ error.message }}
+                  </div>
+                </v-form>
+                <v-card-actions>
+                  <v-spacer />
+                  <v-btn
+                    block
+                    color="#57BFFF"
+                    @click="pressed"
+                    class="white--text"
+                    elevation="0"
+                    x-large
+                    >Login</v-btn
+                  >
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-col> -->
+    </v-row>
+
+    <v-overlay :value="overlay" opacity="0.8">
+      <v-img src="../assets/Spinner.gif"> </v-img>
+    </v-overlay>
   </v-app>
 </template>
 
@@ -127,28 +204,24 @@ export default {
 <style scoped>
 #bglogin {
   height: 100%;
-  width: 100%;
-  background-color: aqua;
-  /* background-image: url("https://images.unsplash.com/photo-1508020963102-c6c723be5764?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"); */
-  /* filter: blur(8px); */
-  background-image: url("../assets/img/Background/SVG/Artboard 1.svg");
+}
+
+#left {
+  /* height: 100%;
+  width: 100%; */
+  /* background-image: url("../assets/PhotonBlue.svg"); */
+  background-image: url("../assets/background/bg1.jpg");
   background-position: center;
+
   background-repeat: no-repeat;
   background-size: cover;
 }
 
-#container {
-  /* height: 100%;
-  width: 100%; */
-  /* background-image: url("../assets/PhotonBlue.svg"); */
-  /* background-image: url("../assets/img/Background/mobile-bg-svg.svg");
+/* #form {
+  background-image: url("../assets/background/bg1.jpg");
   background-position: center;
 
   background-repeat: no-repeat;
-  background-size: cover; */
-}
-
-#card {
-  border-radius: 5%;
-}
+  background-size: cover;
+} */
 </style>
